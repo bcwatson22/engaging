55327b25d9a02200d3d41778fb40444fa1dc53ae
Billy Watson <billy.watson>
2024-11-30 14:37:20 +0000

    SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { type CssVariable } from "next/dist/compiled/@next/font";
 import { Nunito } from "next/font/google";
@@ -20,6 +21,7 @@ const nunito = Nunito({
 const Layout = ({ children }: Layout) => (
   <html lang="en">
     <body className={nunito.className}>{children}</body>
+    <SpeedInsights />
   </html>
 );
 
