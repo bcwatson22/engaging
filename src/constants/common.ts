6074c02d1e2bc24d779d3376881d4dff34b58e3f
Billy Watson <billy.watson>
2025-02-05 22:07:50 +0000

    revalidate

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,4 +1,4 @@
 export const siteName = "Engaging Engineering";
 export const domainName = "https://engaging.engineering";
 export const ogImageUrl = "/api/og";
-export const revalidate = 3600 * 24 * 7;
+export const revalidate = 3600 * 24;
