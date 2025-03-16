9ca25090a631111d4e93a64e82395d7dc2b85dd0
Billy Watson <billy.watson>
2025-03-16 17:12:49 +0000

    undo serverless puppeteer config (with core, not needed)

--- src/utils/saveToPdf.ts
+++ src/utils/saveToPdf.ts
@@ -1,36 +1,9 @@
 import fs from "fs";
-import puppeteer, { type Browser } from "puppeteer";
-import puppeteerCore, { type Browser as BrowserCore } from "puppeteer-core";
-import chromium from "@sparticuz/chromium-min";
+import puppeteer from "puppeteer";
 
 const cssPath = ".next/static/css/";
 const htmlPath = ".next/server/app/cv.html";
 const encoding = "utf-8";
-
-const remoteExecutablePath =
-  "https://github.com/Sparticuz/chromium/releases/download/v133.0.0/chromium-v133.0.0-pack.tar";
-
-let browser: Browser | BrowserCore;
-
-const getBrowser = async () => {
-  if (browser) return browser;
-
-  if (process.env.NODE_ENV === "production") {
-    browser = await puppeteerCore.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath(remoteExecutablePath),
-      headless: true,
-    });
-  } else {
-    browser = await puppeteer.launch({
-      headless: true,
-      args: ["--no-sandbox", "--disable-web-security"],
-      ignoreDefaultArgs: ["--disable-extensions"],
-    });
-  }
-
-  return browser;
-};
 
 const saveToPdf = async () => {
   const htmlContent = fs.readFileSync(htmlPath, encoding);
@@ -40,7 +13,11 @@ const saveToPdf = async () => {
     .filter((filename) => filename.endsWith(".css"));
   const cssContent = fs.readFileSync(cssPath + cssFiles[0], encoding);
 
-  const browser = await getBrowser();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-web-security"],
+    ignoreDefaultArgs: ["--disable-extensions"],
+  });
   const page = await browser.newPage();
 
   await page.setContent(htmlContent, {
